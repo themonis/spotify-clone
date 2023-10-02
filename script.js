e302ca489faccab4ca3dab78cca714045469ebf1
themonis <khanmdmonis45@gmail.com>
2023-10-02 11:56:58 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,15 @@
+console.log("welcome to spotify");
+let song = [
+    {songname: "salam-e-ishq", filepath: "songs/1.mp3", coverpath: "covers/1.jpg"},
+    {songname: "salam-e-ishq", filepath: "songs/1.mp3", coverpath: "covers/1.jpg"},
+    {songname: "salam-e-ishq", filepath: "songs/1.mp3", coverpath: "covers/1.jpg"},
+    {songname: "salam-e-ishq", filepath: "songs/1.mp3", coverpath: "covers/1.jpg"},
+    {songname: "salam-e-ishq", filepath: "songs/1.mp3", coverpath: "covers/1.jpg"},
+    {songname: "salam-e-ishq", filepath: "songs/1.mp3", coverpath: "covers/1.jpg"},
+    {songname: "salam-e-ishq", filepath: "songs/1.mp3", coverpath: "covers/1.jpg"},
+    {songname: "salam-e-ishq", filepath: "songs/1.mp3", coverpath: "covers/1.jpg"},
+    
+]
+
+let audioelement = new Audio('Arcade.mp3');
+// audioelement.play
